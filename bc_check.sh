01649e161b69f98f520dd45f73b5141be7579c38
vpattabhi <40982435+vpattabhi@users.noreply.github.com>
2018-07-22 22:06:52 +0530

    Update bc_check.sh
    just simply

--- bc_check.sh
+++ bc_check.sh
@@ -2,9 +2,10 @@
 ln_pid=`pgrep cpuminer | wc -l`
 if [ $ln_pid  -ne 0 ]
 then
-pid=`pgrep cpuminer`
+#pid=`pgrep cpuminer`
 echo `date` checking pid  $pid >> /home/vallisha/blockchain/check.log
-kill -9 $pid
+#kill -9 $pid
+cat a1.txt
 if [ $? -eq 0 ]
 then
 echo `date` Killed $pid >> /home/vallisha/blockchain/check.log
