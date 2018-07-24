9594ca4a88921336e2461e6a3e83bc4b181f1520
vpattabhi <40982435+vpattabhi@users.noreply.github.com>
2018-07-24 16:53:52 +0530

    Update bc_check.sh

--- bc_check.sh
+++ bc_check.sh
@@ -1,4 +1,16 @@
 #!/bin/sh
+if [ -f /tmp/count ]
+then
+echo "file exists"
+exec_count=`cat /tmp/count`
+exec_count=$(( exec_count +1 ))
+echo $exec_count > /tmp/count
+else
+echo "file does not exist"
+echo "1" > /tmp/count
+fi
+if [ $((exec_count%2)) -eq 0 ]
+then
 ln_pid=`pgrep cpuminer | wc -l`
 if [ $ln_pid  -ne 0 ]
 then
@@ -12,4 +24,7 @@ echo `date` Killed $pid >> /home/vallisha/blockchain/check.log
 fi
 else
 echo `date` Process not running >> /home/vallisha/blockchain/check.log
+fi
+else
+echo `date` "Passing " >> /home/vallisha/blockchain/check.log
 fi
