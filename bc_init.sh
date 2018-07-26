87ad19547378ad8eb9081e8385badb5c291ff6c3
vpattabhi <40982435+vpattabhi@users.noreply.github.com>
2018-07-26 12:50:51 +0530

    Update bc_init.sh

--- bc_init.sh
+++ bc_init.sh
@@ -7,5 +7,5 @@ pid=`pgrep cpuminer`
 echo "Process running not starting again " $pid >> /home/vallisha/blockchain/init.log
 else
 cd /home/vallisha/cpuminer-multi/
-nohup ./cpuminer -a cryptonight -o stratum+tcp://xmr.pool.minergate.com:45700 -u venka_cp@yahoo.co.uk -p x &
+/usr/bin/nohup ./cpuminer -a cryptonight -o stratum+tcp://xmr.pool.minergate.com:45700 -u venka_cp@yahoo.co.uk -p x &
 fi
